163d0a17d1fcbb94eb2579bd18d10c0d0bc86910
apple <2313466338@qq.com>
2018-06-13 19:34:07 +0800

    添加内容

--- app.js
+++ app.js
@@ -1,14 +1,57 @@
 const Koa = require('koa');
-const app = new Koa();
-console.log(app)
-// 对于任何请求，app将调用该异步函数处理请求：
-app.use(async (ctx, next) => {
-    await next();
-    ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, koa2!</h1>';
-});
+const path = require('path')
+const bodyParser = require('koa-bodyparser');
+const ejs = require('ejs');
+const session = require('koa-session-minimal');
+const MysqlStore = require('koa-mysql-session');
+const config = require('./config/config.js');
+const router = require('koa-router')
+const views = require('koa-views')
+// const koaStatic = require('koa-static')
+const staticCache = require('koa-static-cache')
+const app = new Koa()
+
+// session存储配置
+const sessionMysqlConfig= {
+  user: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+}
+
+// 配置session中间件
+app.use(session({
+  key: 'USER_SID',
+  store: new MysqlStore(sessionMysqlConfig)
+}))
 
 
-// 在端口3000监听:
-app.listen(3001);
-console.log('app started at port 3001...');
+// 配置静态资源加载中间件
+// app.use(koaStatic(
+//   path.join(__dirname , './public')
+// ))
+// 缓存
+app.use(staticCache(path.join(__dirname, './public'), { dynamic: true }, {
+  maxAge: 365 * 24 * 60 * 60
+}))
+app.use(staticCache(path.join(__dirname, './images'), { dynamic: true }, {
+  maxAge: 365 * 24 * 60 * 60
+}))
+
+// 配置服务端模板渲染引擎中间件
+app.use(views(path.join(__dirname, './views'), {
+  extension: 'ejs'
+}))
+app.use(bodyParser({
+  formLimit: '1mb'
+}))
+
+//  路由
+
+app.use(require('./routers/signup.js').routes())
+
+
+
+app.listen(3000)
+
+console.log(`listening on port 3000.....`)
--- sql/mysql.js
+++ sql/mysql.js
@@ -29,10 +29,18 @@ let query = ( sql, values ) => {
   })
 }
 
-let createTable = ( sql ) => {
-  return query( sql, [] )
-}
 
+// let query = function( sql, values ) {
+// pool.getConnection(function(err, connection) {
+//   // 使用连接
+//   connection.query( sql,values, function(err, rows) {
+//     // 使用连接执行查询
+//     console.log(rows)
+//     connection.release();
+//     //连接不再使用，返回到连接池
+//   });
+// });
+// }
 
 let users =
     `create table if not exists users(
@@ -44,8 +52,40 @@ let users =
      PRIMARY KEY ( id )
     );`
 
+let posts =
+    `create table if not exists posts(
+     id INT NOT NULL AUTO_INCREMENT,
+     name VARCHAR(100) NOT NULL COMMENT '文章作者',
+     title TEXT(0) NOT NULL COMMENT '评论题目',
+     content TEXT(0) NOT NULL COMMENT '评论内容',
+     md TEXT(0) NOT NULL COMMENT 'markdown',
+     uid VARCHAR(40) NOT NULL COMMENT '用户id',
+     moment VARCHAR(100) NOT NULL COMMENT '发表时间',
+     comments VARCHAR(200) NOT NULL DEFAULT '0' COMMENT '文章评论数',
+     pv VARCHAR(40) NOT NULL DEFAULT '0' COMMENT '浏览量',
+     avator VARCHAR(100) NOT NULL COMMENT '用户头像',
+     PRIMARY KEY(id)
+    );`
+
+let comment =
+    `create table if not exists comment(
+     id INT NOT NULL AUTO_INCREMENT,
+     name VARCHAR(100) NOT NULL COMMENT '用户名称',
+     content TEXT(0) NOT NULL COMMENT '评论内容',
+     moment VARCHAR(40) NOT NULL COMMENT '评论时间',
+     postid VARCHAR(40) NOT NULL COMMENT '文章id',
+     avator VARCHAR(100) NOT NULL COMMENT '用户头像',
+     PRIMARY KEY(id) 
+    );`
+
+let createTable = ( sql ) => {
+  return query( sql, [] )
+}
+
+// 建表
 createTable(users)
-console.log(createTable(users));
+createTable(posts)
+createTable(comment)
 
 // 注册用户
 exports.insertData = ( value ) => {
@@ -61,4 +101,103 @@ exports.deleteUserData = ( name ) => {
 exports.findUserData = ( name ) => {
   let _sql = `select * from users where name="${name}";`
   return query( _sql )
+}
+// 发表文章
+exports.insertPost = ( value ) => {
+  let _sql = "insert into posts set name=?,title=?,content=?,md=?,uid=?,moment=?,avator=?;"
+  return query( _sql, value )
+}
+// 更新文章评论数
+exports.updatePostComment = ( value ) => {
+  let _sql = "update posts set comments=? where id=?"
+  return query( _sql, value )
+}
+
+// 更新浏览数
+exports.updatePostPv = ( value ) => {
+  let _sql = "update posts set pv=? where id=?"
+  return query( _sql, value )
+}
+
+// 发表评论
+exports.insertComment = ( value ) => {
+  let _sql = "insert into comment set name=?,content=?,moment=?,postid=?,avator=?;"
+  return query( _sql, value )
+}
+// 通过名字查找用户
+exports.findDataByName =  ( name ) => {
+  let _sql = `select * from users where name="${name}";`
+  return query( _sql)
+}
+// 通过文章的名字查找用户
+exports.findDataByUser =  ( name ) => {
+  let _sql = `select * from posts where name="${name}";`
+  return query( _sql)
+}
+// 通过文章id查找
+exports.findDataById =  ( id ) => {
+  let _sql = `select * from posts where id="${id}";`
+  return query( _sql)
+}
+// 通过文章id查找
+exports.findCommentById =  ( id ) => {
+  let _sql = `select * from comment where postid="${id}";`
+  return query( _sql)
+}
+// 通过评论id查找
+exports.findComment =  ( id ) => {
+  let _sql = `select * from comment where id="${id}";`
+  return query( _sql)
+}
+
+// 查询所有文章
+exports.findAllPost =  () => {
+  let _sql = ` select * from posts;`
+  return query( _sql)
+}
+// 查询分页文章
+exports.findPostByPage =  ( page ) => {
+  let _sql = ` select * from posts limit ${(page-1)*10},10;`
+  return query( _sql)
+}
+// 查询个人分页文章
+exports.findPostByUserPage =  (name,page) => {
+  let _sql = ` select * from posts where name="${name}" order by id desc limit ${(page-1)*10},10 ;`
+  return query( _sql)
+}
+// 更新修改文章
+exports.updatePost = (values) => {
+  let _sql = `update posts set title=?,content=?,md=? where id=?`
+  return query(_sql,values)
+}
+// 删除文章
+exports.deletePost = (id) => {
+  let _sql = `delete from posts where id = ${id}`
+  return query(_sql)
+}
+// 删除评论
+exports.deleteComment = (id) => {
+  let _sql = `delete from comment where id=${id}`
+  return query(_sql)
+}
+// 删除所有评论
+exports.deleteAllPostComment = (id) => {
+  let _sql = `delete from comment where postid=${id}`
+  return query(_sql)
+}
+// 查找评论数
+exports.findCommentLength = (id) => {
+  let _sql = `select content from comment where postid in (select id from posts where id=${id})`
+  return query(_sql)
+}
+
+// 滚动无限加载数据
+exports.findPageById = (page) => {
+  let _sql = `select * from posts limit ${(page-1)*5},5;`
+  return query(_sql)
+}
+// 评论分页
+exports.findCommentByPage = (page,postId) => {
+  let _sql = `select * from comment where postid=${postId} order by id desc limit ${(page-1)*10},10;`
+  return query(_sql)
 }
